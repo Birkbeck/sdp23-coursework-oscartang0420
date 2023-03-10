675890f48561a33ea85a8249c86eb9e896854337
oscartang0420 <122899657+oscartang0420@users.noreply.github.com>
2023-03-10 20:51:56 +0000

    Add files via upload

--- src/sml/instruction/AddInstruction.java
+++ src/sml/instruction/AddInstruction.java
@@ -3,16 +3,15 @@ package sml.instruction;
 import sml.Instruction;
 import sml.Machine;
 import sml.RegisterName;
-
-// TODO: write a JavaDoc for the class
+import java.util.Objects;
 
 /**
- * @author
+ *
  */
-
 public class AddInstruction extends Instruction {
 	private final RegisterName result;
 	private final RegisterName source;
+
 
 	public static final String OP_CODE = "add";
 
@@ -23,15 +22,34 @@ public class AddInstruction extends Instruction {
 	}
 
 	@Override
-	public int execute(Machine m) {
-		int value1 = m.getRegisters().get(result);
-		int value2 = m.getRegisters().get(source);
-		m.getRegisters().set(result, value1 + value2);
+	public int execute(Machine machine) {
+		int value1 = machine.getRegisters().get(result);
+		int value2 = machine.getRegisters().get(source);
+		machine.getRegisters().set(result, value1 + value2);
 		return NORMAL_PROGRAM_COUNTER_UPDATE;
 	}
 
+
+
 	@Override
 	public String toString() {
-		return getLabelString() + getOpcode() + " " + result + " " + source;
+		return getLabelString() +getOpcode() + " " + result + " " + source;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(label, result, source);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		if (!(o instanceof AddInstruction other))return false;
+		return Objects.equals(this.label, other.label)
+				&& Objects.equals(this.result, other.result)
+				&& Objects.equals(this.source, other.source);
 	}
 }
+
+
---
+++ src/sml/instruction/SubInstruction.java
@@ -0,0 +1,52 @@
+package sml.instruction;
+
+import sml.Instruction;
+import sml.Machine;
+import sml.RegisterName;
+import java.util.Objects;
+
+/**
+ * This class represents the "sub" instruction in SML.
+ * The instruction subtracts the value in the second register from the value in the first register
+ * and stores the result in the first register.
+ */
+public class SubInstruction extends Instruction {
+    private final RegisterName result;
+    private final RegisterName source;
+
+    public static final String OP_CODE = "sub";
+
+    public SubInstruction(String label, RegisterName result, RegisterName source) {
+        super(label, OP_CODE);
+        this.result = result;
+        this.source = source;
+    }
+
+    @Override
+    public int execute(Machine m) {
+        int value1 = m.getRegisters().get(result);
+        int value2 = m.getRegisters().get(source);
+        m.getRegisters().set(result, value1 - value2);
+        return NORMAL_PROGRAM_COUNTER_UPDATE;
+    }
+
+    @Override
+    public String toString() {
+        return getLabelString() + getOpcode() + " " + result + " " + source;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(label, result, source);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!(o instanceof SubInstruction other))return false;
+        return Objects.equals(this.label, other.label)
+                && Objects.equals(this.result, other.result)
+                && Objects.equals(this.source, other.source);
+    }
+}
